b3c2533baf5e77135852ad8fb1b143da4f0edaa3
Anderson Porto <anderson.higo2@gmail.com>
2023-04-01 15:10:28 -0300

    refactor: basic crud struct

--- src/main/java/dev/anderson/resource/BookResource.java
+++ src/main/java/dev/anderson/resource/BookResource.java
@@ -4,10 +4,7 @@ import dev.anderson.entities.BookEntity;
 import dev.anderson.service.BookService;
 import org.jboss.resteasy.reactive.ResponseStatus;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/api/v1/livraria/livros")
@@ -31,5 +28,26 @@ public class BookResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getAll() {
         return bookService.getAllBooks().toString();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public BookEntity getBook(@PathParam("id") Long id) {
+        return bookService.getBook(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public BookEntity updateBook(@PathParam("id") Long id, BookEntity book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @ResponseStatus(204)
+    public void deleteBook(@PathParam("id") Long id) {
+        bookService.deleteBook(id);
     }
 }
